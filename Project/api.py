675db4e2b08e0d9aa3ef5b45a2445855a9023536
Alexander Daniel <146755389+Lexxxxxa2@users.noreply.github.com>
2026-01-08 11:20:25 +0700

    Delete Project/api.py

--- Project/api.py
+++
@@ -1,9 +0,0 @@
-# api.py
-from flask_restx import Api
-
-api = Api(
-    title="API Manajemen Butik Pakaian",
-    version="1.0",
-    description="Dokumentasi API Backend Sistem Manajemen Butik Pakaian",
-    doc="/"   # Swagger UI muncul di /
-)
